576d97a32f166a529fc102d79cf0801cb8767531
Yassin-ck <mohammedyassinck@gmail.com>
2023-05-31 14:04:14 +0530

    update

--- validation/forms.py
+++
@@ -1,2 +0,0 @@
-from django.forms import forms
-
--- validation/views.py
+++ validation/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 def Register(request):
@@ -10,10 +11,12 @@ def Register(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        cPassword = request.POST.get('cpassword')
+        cPassword = request.POST.get('cpassword') 
+    
         if password != cPassword:
-            return HttpResponse("Password doesn't match")
+            messages.error(request,"Password doesn't match")
         else:
+           print('hello')
            my_user = User.objects.create_user(username,email,password)
            my_user.save()
            return redirect('login')
@@ -23,19 +26,21 @@ def Register(request):
 def Login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
-        print(username)
         password = request.POST.get('password')
-        print(password)
         user = authenticate(request,username=username,password=password)
-        print(user)
         if user is not None:
             login(request,user)
+            print('hii')
             return redirect ('home')
         else:
-            return HttpResponse('Email or Password not Correct')
+          messages.error(request,'Username or Password not Correct')
+       
+         
+    return render(request,'Login.html')
+        
         
 
-    return render(request,'Login.html')
+
 @login_required(login_url='login')
 def Home(request):
     return render(request,'home.html')
